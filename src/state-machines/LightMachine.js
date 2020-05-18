af991434387eeb842936fe2717f5ada79a09f250
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-17 19:42:58 -0700

    Completed wave 1

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -2,7 +2,27 @@ import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
   // Your state machine blueprint here
-
+  id: 'toggler',
+  initial: "red",
+  states: {
+    red: {
+      on: {
+        changeLight: 'green',
+      }
+    },
+    
+    green: {
+      on: {
+      changeLight: 'yellow',
+      }
+    },
+    
+    yellow: {
+      on: {
+      changeLight: 'red',
+      }
+    },
+  }
 });
 
 
